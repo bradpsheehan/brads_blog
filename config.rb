b8b0a2933dcab4e704a05c9e203230ea9a221b31
Bradley Sheehan <bradpsheehan@gmail.com>
2013-02-11 08:13:15 -0700

    Add timezone

--- config.rb
+++ config.rb
@@ -14,6 +14,8 @@ require "zurb-foundation"
 # Page options, layouts, aliases and proxies
 ###
 
+Time.zone = 'US/Mountain'
+
 activate :blog do |blog|
   # blog.prefix = "blog"
   # blog.permalink = ":year/:month/:day/:title.html"
